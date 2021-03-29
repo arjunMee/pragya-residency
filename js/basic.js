1c7142e1b97bf03eb4477f50a8b57d7d9b607112
arjunMee <70093722+arjunMee@users.noreply.github.com>
2021-03-30 00:01:43 +0530

    Add files via upload

--- js/basic.js
+++ js/basic.js
@@ -17,8 +17,42 @@ navItem.forEach((e)=>{
     })
 }) 
 
-/*===== SWIPER CAROUSEL =====*/ 
-const mySwiper = new Swiper('.swiper-container', {
+// ===================
+// slider
+// ==================
+let totalSlide = document.getElementsByClassName('slider');
+
+function removeAll(){
+     for(let i=0; i < totalSlide.length; i++){
+        totalSlide[i].classList.remove('slider__active');
+     }
+}
+let current = 0;
+let right = document.getElementById('right').addEventListener('click', function(){
+    removeAll()
+    if(current === totalSlide.length - 1)
+    {
+        current = 0;
+    }else{
+        current++;
+    }
+    totalSlide[current].classList.add('slider__active');
+})
+
+let left = document.getElementById('left').addEventListener('click', function(){
+    removeAll()
+    if(current === 0)
+    {
+        current = totalSlide.length - 1;
+    }else{
+        current--;
+    }
+    totalSlide[current].classList.add('slider__active');
+})
+
+
+
+const mySwiper1 = new Swiper('.block-2', {
     loop: true,
     grabCursor: true,
     centeredSlides: true,
